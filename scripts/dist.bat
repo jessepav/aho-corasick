1343266e2157361d88633fbbe58ba2a5d44d06ef
Jesse Pavel <jpavel@alum.mit.edu>
2020-09-17 20:41:41 -0400

    Fix dist.bat

--- scripts/dist.bat
+++ scripts/dist.bat
@@ -9,7 +9,7 @@ IF NOT EXIST dist mkdir dist
 call ant compile.module.aho-corasick.production
 call scripts\make-javadoc.cmd
 
-jar -cvf dist\jessepav-ahocorasick.jar -C build\classes\aho-corasick org 
+jar -cvf dist\jessepav-ahocorasick.jar -C build\classes\aho-corasick . 
 jar -cvf dist\jessepav-ahocorasick-javadoc.jar -C build\javadoc . 
 jar -cvf dist\jessepav-ahocorasick-source.jar -C src\main\java . 
 
